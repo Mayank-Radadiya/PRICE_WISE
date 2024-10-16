f9ca207c6535eeba8244340f6a4fb384d9d4f4eb
Mayank-Radadiya <mayankradadiya2@gmail.com>
2024-10-16 08:30:25 +0530

    :)

--- components/ProductCard.tsx
+++ components/ProductCard.tsx
@@ -6,32 +6,41 @@ import { Product } from "@/types";
 interface Props {
   product: Product;
 }
-function ProductCard({ product }:Props) {
-   return (
-     <Link href={`/products/${product._id}`} className="product-card">
-       <div className="product-card_img-container">
-         <Image
-           src={product.image}
-           alt={product.title}
-           width={200}
-           height={200}
-           className="product-card_img"
-         />
-       </div>
+function ProductCard({ product }: Props) {
+  return (
+    <Link href={`/products/${product._id}`} className="product-card">
+      <div className="product-card_img-container">
+        <Image
+          src={product.image}
+          alt={product.title}
+          width={200}
+          height={200}
+          className="product-card_img"
+        />
+      </div>
 
-       <div className="flex flex-col gap-3">
-         <h3 className="product-title">{product.title}</h3>
+      <div className="flex flex-col gap-3">
+        <h3 className="product-title">{product.title}</h3>
 
-         <div className="flex justify-between">
+        <div className="flex justify-between">
+          <p className="text-black opacity-50 text-lg capitalize">//</p>
 
-           <p className="text-black text-lg font-semibold">
-             <span>{product?.currency}</span>
-             <span>{product?.currentPrice}</span>
-           </p>
-         </div>
-       </div>
-     </Link>
-   );
+          <p className="text-black text-lg font-semibold">
+            {product.outOfStock == true ? (
+              <h3 className="text-1xl font-semibold text-red-600">
+                Out of stock.
+              </h3>
+            ) : (
+              <>
+                <span>{product?.currency}</span>
+                <span>{product?.currentPrice}</span>
+              </>
+            )}
+          </p>
+        </div>
+      </div>
+    </Link>
+  );
 }
 
 export default ProductCard;
